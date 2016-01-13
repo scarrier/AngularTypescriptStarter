a9b85c659a09fad91cdcb27ad5e5faa759e1c90e
Scarrier <scarrier@censeohealth.com>
2016-01-13 15:55:36 -0600

    Cleanup files and fix .gitignore

---
+++ settings.js
@@ -0,0 +1,8 @@
+﻿"use strict";
+var settings = {
+    //Put any application settings here, they will be substituted on a local build with the appSettings properties in web.config
+    //On a remote build, octopus will do the substitution
+    securityApiUrl: '#{SecurityApiUrl}',
+    versionNumber: '1.0.0.0'
+};
+window.applicationSettings = settings;
